7935422dbde4e693d426b7347478c43f3f088858
Michael Li <lvlichaelly@gmail.com>
2017-05-16 21:47:56 -0700

    [add] server independent (non-redis for simplicity) cache

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,35 +1,47 @@
 const axios = require('axios');
-const Store = require('../store/store');
+// const Store = require('../store/store'); // simple in memory cache
+
+const cache = require('persistent-cache')(); // server independent cache
 
 exports.fetchArtists = (req, res, next) => {
-  // use server simple cache first
   const artist = req.query.artist;
-  if (Store.artists[artist]) {
-    console.log('Found cached artist: ', artist);
-    return res.send(Store.artists[artist]);
-  }
-  console.log('searching by artist: ', artist);
-  // if no cache, query spotify
-  axios
-    .get('https://api.spotify.com/v1/search', { params: { q: artist, type: 'artist'} })
-    .then(resp => {
-      Store.artists[artist] = resp.data.artists.items;
-      return res.send(resp.data.artists.items);
-    })
-    .catch((err) => {
-      console.log("500 Error: ", err);
-      res.status(500).send({ error: err });
-    });
+  cache.get(artist, function(err, value) {
+    if (!err) {
+      if (value === undefined) { // query spotify and cache the result
+        console.log('searching by artist: ', artist);
+        axios
+          .get('https://api.spotify.com/v1/search', { params: { q: artist, type: 'artist'} })
+          .then(resp => {
+            cache.put(artist, resp.data.artists.items, function() {
+              res.send(resp.data.artists.items);
+            })
+          })
+          .catch((err) => {
+            console.log("500 Error: ", err);
+            res.status(500).send({ error: err });
+          });
+      } else { // send value from cache
+        console.log('Found cached artist: ', artist);
+        return res.send(value);
+      }
+    }
+  });
 }
 
 
 exports.fetchArtistsByGenres = (req, res, next) => {
   const genres = req.query.genres;
-  // use server simple cache first
-  if (Store.genres[genres]) {
-    console.log('Found cached genres: ', genres);
-    return res.send(Store.genres[genres]);
-  }
+  cache.get(genres, function(err, value) {
+    if (!err) {
+      if (value === undefined) { // query spotify and cache the result
+        recursiveSearch(genres);
+      } else { // send value from cache
+        console.log('Found cached genres: ', genres);
+        res.send(value);
+      }
+    }
+  });
+
   // if no cache, query spotify
   let artists = [];
   const recursiveSearch = (genres) => {
@@ -42,8 +54,9 @@ exports.fetchArtistsByGenres = (req, res, next) => {
       .then(resp => {
         artists = artists.concat(resp.data.artists.items)
         if (artists.length > 0) {
-          Store.genres[genres] = artists;
-          return res.send(artists);
+          cache.put(genres, artists, function() {
+            res.send(artists);
+          })
         } else {
           return recursiveSearch(genres.slice(1));
         }
@@ -53,5 +66,4 @@ exports.fetchArtistsByGenres = (req, res, next) => {
         res.status(500).send({ error: err });
       });
   }
-  recursiveSearch(genres);
 }
